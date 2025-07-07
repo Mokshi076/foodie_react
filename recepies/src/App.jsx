3fca1fb5d537dc4b95f7ded0ebdeba30a030a653
Mokshi <mokshi104rajak@gmail.com>
2025-07-07 17:05:03 +0530

    setup

---
+++ recepies/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+
+
+function App() {
+
+
+  return (
+    <>
+    <Header/>  
+    <h1 className="bg-gradient-to-r from-amber-500 to-orange-500 text-white">
+      sudfdbobd
+    </h1>
+
+    </>
+  );
+}
+
+export default App;
+
+const Header = () =>{
+  <header className= "bg-gradient-to-r from-amber-500 to-orange-500 text-white shadow-md fixed top-0 flex-0 w-full z-10">
+    HEADER
+  </header>
+};
